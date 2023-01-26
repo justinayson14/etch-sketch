7a711019a1e716adcf99fef377434d2258ebf8c4
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2023-01-25 23:23:36 -0800

    Add funcs to draw in container

--- pew.js
+++ pew.js
@@ -1,29 +1,43 @@
 const container = document.querySelector('.box-container');
+const clear = document.querySelector('.clear-button');
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.className = 'row-of-boxes';
-    for (let s = 0; s < 16; s++) {
-        const box = document.createElement('div');
-        box.className = 'box-in-container';
-        row.appendChild(box);
+const boxDim = prompt("What dimensions of the grid would you like? Please enter a value below or equal to 100.");
+checkBoxDim(boxDim);
+
+clear.addEventListener('click', clearContainer);
+console.log(container.childNodes);
+
+function makeSquares(lengthSide) {
+    for (let i = 0; i < lengthSide; i++) {
+        const row = document.createElement('div');
+        row.className = 'row-of-boxes';
+        for (let s = 0; s < lengthSide; s++) {
+            const box = document.createElement('div');
+            box.className = 'box-in-container';
+            box.style.width = 600/lengthSide + 'px';
+            box.style.height = 600/lengthSide + 'px';
+            box.addEventListener('mouseover', fillBox);
+            row.appendChild(box);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
-
-container.addEventListener('click', checkNode);
-
-container.addEventListener('click', fillBox);
-container.addEventListener('dblclick', unfillBox);
 
 function fillBox(e) {
-    e.target.setAttribute('style', 'background-color: #0D1B1E');
+    e.target.style.backgroundColor = '#0D1B1E';
 }
 
-function unfillBox(e) {
-    e.target.setAttribute('style', 'background-color: transparent');
+function clearContainer(e) {
+    const boxes = document.querySelectorAll('.box-in-container');
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].style.backgroundColor = 'transparent';
+    }
 }
 
-function checkNode(e) {
-    console.log(e);
+function checkBoxDim(userVal) {
+   while(userVal > 100) {
+    alert(`${userVal} is invalid. Please try again`);
+    userVal = prompt('Enter value below or equal to 100: ');
+   }
+   return makeSquares(userVal);
 }
