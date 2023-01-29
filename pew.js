61cfc83b183decaafd32be014f1cd1dd334a99b4
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2023-01-29 02:36:20 -0800

    Program buttons to change color of background color for squares

--- pew.js
+++ pew.js
@@ -1,33 +1,38 @@
 const container = document.querySelector('.box-container');
-const clear = document.querySelector('.clear-button');
+const clear = document.querySelector('#clear-btn');
+const black = document.querySelector('#blk-btn');
+const rgb = document.querySelector('#rgb-btn');
+const shade = document.querySelector('#shade-btn');
 
 const boxDim = prompt("What dimensions of the grid would you like? Please enter a value below or equal to 100.");
 checkBoxDim(boxDim);
 
 clear.addEventListener('click', clearContainer);
-console.log(container.childNodes);
+black.addEventListener('click', chooseBlack);
+rgb.addEventListener('click', chooseRGB);
+shade.addEventListener('click', chooseShade);
 
 function makeSquares(lengthSide) {
-    for (let i = 0; i < lengthSide; i++) {
+    for(let i = 0; i < lengthSide; i++) {
         const row = document.createElement('div');
         row.className = 'row-of-boxes';
-        for (let s = 0; s < lengthSide; s++) {
+        for(let s = 0; s < lengthSide; s++) {
             const box = document.createElement('div');
             box.className = 'box-in-container';
             box.style.width = 600/lengthSide + 'px';
             box.style.height = 600/lengthSide + 'px';
-            box.addEventListener('mouseover', fillBox);
             row.appendChild(box);
         }
         container.appendChild(row);
     }
 }
 
-function fillBox(e) {
-    e.target.style.backgroundColor = '#0D1B1E';
+function fillBox(e, color = 'white') {
+    color = pickColor;
+    e.target.style.backgroundColor = color;
 }
 
-function clearContainer(e) {
+function clearContainer() {
     const boxes = document.querySelectorAll('.box-in-container');
     for (let i = 0; i < boxes.length; i++) {
         boxes[i].style.backgroundColor = 'transparent';
@@ -35,9 +40,38 @@ function clearContainer(e) {
 }
 
 function checkBoxDim(userVal) {
-   while(userVal > 100) {
+   while(userVal > 100 || userVal <= 0) {
     alert(`${userVal} is invalid. Please try again`);
     userVal = prompt('Enter value below or equal to 100: ');
    }
    return makeSquares(userVal);
+}
+
+function chooseBlack(e) {
+    const boxes = document.querySelectorAll('.box-in-container');
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener('mouseover', (e) => e.target.style.backgroundColor = 'black');
+    }
+}
+
+function chooseRGB(e) {
+    const boxes = document.querySelectorAll('.box-in-container');
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener('mouseover', (e) => e.target.style.backgroundColor = `rgb(${randomizeNum()}, ${randomizeNum()}, ${randomizeNum()})`);
+    }
+}
+
+function chooseShade(e) {
+    const boxes = document.querySelectorAll('.box-in-container');
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener('mouseover', (e) => e.target.style.backgroundColor = `hsl(191, 40%, ${randomizeShade()}%)`);
+    }
+}
+
+function randomizeNum() {
+    return Math.floor(Math.random() * 255);
+}
+
+function randomizeShade() {
+    return Math.floor(Math.random() * 40 + 10);
 }
